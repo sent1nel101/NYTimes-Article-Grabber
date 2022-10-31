c7f0c2a028b4062f5197709e8cd8844058490ae5
Darec McDaniel <nght.crwlr@yahoo.com>
2022-10-30 23:29:43 -0400

    Add files via upload
    React app that allows the user to enter a search term and pullss relevant headlines frrom the NY Times open API.
    Displays a headline (which can be clicked to view the full article at NY Times website), a snippet of the article, and a relevant thumbnail image.

---
+++ api-fetch-practice/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Module from './components/Module';
+import ModuleTwo from './components/ModuleTwo';
+
+function App() {
+  return (
+    <div className="App">
+      <Module />
+      <ModuleTwo />
+    </div>
+  );
+}
+
+export default App;
---
+++ api-fetch-practice/src/components/Module.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Module = () => {
+  return (
+    <div><h1><strong>New York Times Headline Searcher</strong></h1></div>
+  )
+}
+
+export default Module
